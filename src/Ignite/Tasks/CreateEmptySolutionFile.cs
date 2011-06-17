9ba69b31aa81d4bbde0533b6d895f4e56e861e7d
Mogens Heller Grabe <mookid8000@gmail.com>
2011-06-18 00:53:44 +0200

    MSBuild script is now included among the solution items

--- src/Ignite/Tasks/CreateEmptySolutionFile.cs
+++ src/Ignite/Tasks/CreateEmptySolutionFile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Text;
 
@@ -16,15 +17,24 @@ namespace Ignite.Tasks
 
         public override void Execute()
         {
-            const string slnVs2010 = @"
+            var solutionItemsProjectId = Guid.NewGuid();
+
+            var slnVs2010 =
+                @"
 Microsoft Visual Studio Solution File, Format Version 11.00
 # Visual Studio 2010
+Project(""{2150E333-8FDC-42A3-9474-1A3956D46DE8}"") = ""Solution Items"", ""Solution Items"", ""{solutionItemsProjectId}""
+	ProjectSection(SolutionItems) = preProject
+		..\scripts\build.proj = ..\scripts\build.proj
+	EndProjectSection
+EndProject
 Global
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
 	EndGlobalSection
 EndGlobal
-";
+"
+                    .Replace("{solutionItemsProjectId}", solutionItemsProjectId.ToString("B").ToUpper());
 
             var filename = GetFilename();
             Info("Generating empty VS2010 solution file: {0}", filename);
